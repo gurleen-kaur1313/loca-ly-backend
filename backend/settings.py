f60891a1b872b54ebe6a4f18d247fced832376bd
Gurleen <gurleendhillonm1114@gmail.com>
2021-02-19 16:48:48 +0530

    pg model

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'Profile',
     'job',
     'security',
+    'pg',
 ]
 
 
--- pg/models.py
+++ pg/models.py
@@ -1,3 +1,50 @@
+from django.conf import settings
 from django.db import models
+import uuid
+
+
+class Pgs(models.Model):
+    USERTYPE_CHOICES = (
+        ("B", "BOYS"),
+        ("G", "GIRLS"),
+        ("O", "BOTH"),
+    )
+    KITCHEN_CHOICES = (
+        ("Y", "Yes"),
+        ("N", "No"),
+    )
+    WASHROOM_CHOICES = (
+        ("Y", "Yes"),
+        ("N", "No"),
+    )
+    LAUNDRY_CHOICES = (
+        ("Y", "Yes"),
+        ("N", "No"),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    usertype = models.CharField(
+        max_length=250, choices=USERTYPE_CHOICES, blank=True, null=True
+    )
+    kitchen_available = models.CharField(
+        max_length=250, choices=KITCHEN_CHOICES, blank=True, null=True
+    )
+    washroom_attached = models.CharField(
+        max_length=250, choices=WASHROOM_CHOICES, blank=True, null=True
+    )
+    laundry_included = models.CharField(
+        max_length=250, choices=LAUNDRY_CHOICES, blank=True, null=True
+    )
+    rent = models.IntegerField(null=True,blank=True)
+    locality = models.CharField(null=True,blank=True,max_length=250)
+    city = models.CharField(null=True,blank=True,max_length=250)
+    state = models.CharField(null=True,blank=True,max_length=250)
+
+    def __str__(self):
+        return self.locality
+
+    def save(self, *args, **kwargs):
+        super(Pgs, self).save(*args, **kwargs)
+
+
 
 
