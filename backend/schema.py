7bd90af06cbeda35cc9682b81c5dc7956bfbc9d7
Harmanjit Singh <harmanjit140500@gmail.com>
2021-02-20 10:01:17 +0530

    gql setting

--- backend/schema.py
+++ backend/schema.py
@@ -3,13 +3,14 @@ import graphql_jwt
 import job.schema
 import Profile.schema
 import security.schema
+import pg.schema
 
 
-class Query(job.schema.Query,Profile.schema.Query,security.schema.Query, graphene.ObjectType):
+class Query(job.schema.Query, Profile.schema.Query, security.schema.Query, pg.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(job.schema.Query,Profile.schema.Query,security.schema.Query, graphene.ObjectType):
+class Mutation(job.schema.Mutation, Profile.schema.Mutation, security.schema.Mutation, pg.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
