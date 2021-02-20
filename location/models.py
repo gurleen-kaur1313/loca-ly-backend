f1551561f139f246ca4acd46b378ffdad4d4523a
Harmanjit Singh <harmanjit140500@gmail.com>
2021-02-20 21:07:34 +0530

    error++

--- location/models.py
+++ location/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Location(models.Model):
-    city = models.CharField(null=True,blank=True,max_length=250)
-    state = models.CharField(null=True,blank=True,max_length=250)
+    city = models.CharField(null=True, blank=True, max_length=250)
+    state = models.CharField(null=True, blank=True, max_length=250)
     rating = models.IntegerField(null=True, blank=True, default=0)
 
     def __str__(self):
@@ -10,4 +11,3 @@ class Location(models.Model):
 
     def save(self, *args, **kwargs):
         super(Location, self).save(*args, **kwargs)
-
--- location/schema.py
+++ location/schema.py
@@ -11,18 +11,16 @@ class locations(DjangoObjectType):
     class Meta:
         model = Location
 
+
 class Query(graphene.ObjectType):
     alllocations = graphene.List(locations)
     onelocation = graphene.Field(locations, location_city=graphene.String())
 
-    def resolve_alllocations(self,info):
+    def resolve_alllocations(self, info):
         return Location.objects.all()
-
 
     def resolve_by_id(root, info, location_city):
         return Location.objects.get(city=location_city)
-
-
 
 
 class IncrementLocation(graphene.Mutation):
@@ -32,18 +30,18 @@ class IncrementLocation(graphene.Mutation):
         city = graphene.String()
         rating = graphene.Int()
 
-    def mutate(self, info,city,rating,**kwargs):
+    def mutate(self, info, city, rating, **kwargs):
         user = info.context.user
         if user.is_anonymous:
-            raise GraphQLError("Not Logged In!")   
+            raise GraphQLError("Not Logged In!")
         try:
             if city:
                 filter = Q(city__icontains=city)
             temp = Location.objects.filter(filter).first()
-            temp.rating+=rating
+            temp.rating += rating
             temp.save()
         except:
-            temp=Location.objects.create(city=city)
+            temp = Location.objects.create(city=city)
             temp.save()
         return IncrementLocation(newlocation=temp)
 
@@ -55,7 +53,7 @@ class DecrementLocation(graphene.Mutation):
         city = graphene.String()
         rating = graphene.Int()
 
-    def mutate(self, info,city,rating,**kwargs):
+    def mutate(self, info, city, rating, **kwargs):
         user = info.context.user
         if user.is_anonymous:
             raise GraphQLError("Not Logged In!")
@@ -63,14 +61,12 @@ class DecrementLocation(graphene.Mutation):
             if city:
                 filter = Q(city__icontains=city)
             temp = Location.objects.filter(filter).first()
-            temp.rating-=rating
+            temp.rating -= rating
             temp.save()
         except:
-            temp=Location.objects.create(city=city)
+            temp = Location.objects.create(city=city)
             temp.save()
         return DecrementLocation(newlocation=temp)
-
-        
 
 
 class Mutation(graphene.ObjectType):
